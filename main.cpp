9bf287eb1cdd829cbc7c77fc08f5fc0fc4db9044
VISHAL BHARTI <42848830+knightvishal@users.noreply.github.com>
2020-10-31 23:55:02 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,18 @@ using namespace std;
 string imie, nazwisko;
 int nr_tel;
 
+     
+void printFibonacci(int n){    
+    static int n1=0, n2=1, n3;    
+    if(n>0){    
+         n3 = n1 + n2;    
+         n1 = n2;    
+         n2 = n3;    
+ cout<<n3<<" ";    
+         printFibonacci(n-1);    
+    }    
+} 
+
 int main()
 {
     cout << "Podaj imie: ";         cin >> imie;
@@ -21,4 +33,14 @@ int main()
     plik.close();
 
     return 0;
+   
+   
+    int n;    
+    cout<<"Enter the number of elements: ";    
+    cin>>n;    
+    cout<<"Fibonacci Series: ";    
+    cout<<"0 "<<"1 ";  
+    printFibonacci(n-2);  //n-2 because 2 numbers are already printed    
+     return 0;  
+
 }
